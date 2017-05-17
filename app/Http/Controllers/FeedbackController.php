0bd36e8c0b388277590c4c7a8cb94dfa0627a02b
TasneemElSayedAllam <tasneemelsaydalam@gmail.com>
2017-05-17 18:38:32 +0200

    Create Feedback Model

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Request;
 
 class FeedbackController extends Controller
 {
@@ -14,6 +14,8 @@ class FeedbackController extends Controller
     }
     
 	public function store(){
+
+
 
 
 	}
