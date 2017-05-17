50ffcf6d241141c1e50cf03e0048195a7009077f
Ranaazmy <rana.azmy95@hotmail.com>
2017-05-17 18:58:15 +0200

    Update Post View

--- resources/views/Form/postform.blade.php
+++ resources/views/Form/postform.blade.php
@@ -9,12 +9,11 @@
 				<div class="panel-heading"><h1 style= "color : Black;"><strong>Write a Post<strong/></h1></div>
 				<br/>
 				<div class="panel-body">
-
-
-		{!! Form::open(['url' => 'post'])!!}
-			{!! Form::textarea('post',null )!!}
-			<br/>
-			{!! Form::submit('Submit', ['class' => 'btn btn-primary'])!!}
-		{!! Form::close()!!}
+				{!! Form::open(['url' => 'post'])!!}
+					{!! Form::textarea('post',null )!!}
+					<br/>
+					{!! Form::submit('Submit', ['class' => 'btn btn-primary'])!!}
+				{!! Form::close()!!}
+				</div>
 
 @stop
