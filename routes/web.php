1dfd0969e42c083effc828ec2e18ede62b1380b1
Basma Adrii <basma.adrii@gmail.com>
2017-05-17 05:59:28 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
-
-Route::auth();
-
-Route::get('/',function(){return view('welcome');});
-Route::get('/partnership', 'PagesController@partnership');
-Route::post('/partnership', 'PagesController@storepartner');
-
-Route::get('/feedback', 'PagesController@feedback');
-Route::post('/feedback', 'PagesController@storefeedback');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/',function(){return view('welcome');});
+Route::get('/partnership', 'PartnershipController@display');
+Route::post('/partnership', 'PartnershipController@store');
+
+Route::get('/feedback', 'FeedbackController@display');
+Route::post('/feedback', 'FeedbackController@store');
+
