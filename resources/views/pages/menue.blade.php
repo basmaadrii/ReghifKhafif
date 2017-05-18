00c46f8020b44b5a87803684f582a70359075a99
Ranaazmy <rana.azmy95@hotmail.com>
2017-05-18 05:52:45 +0200

    Create Menue Page

--- resources/views/pages/menue.blade.php
+++ resources/views/pages/menue.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.app')
 @section('content')
 
-
-
-
-
-
+{!! Html::image('images/1.png', null, array('class' => 'img-responsive col-md-5 col-md-offset-4')) !!}
+<br/>
+<br/>
+{!! Html::image('images/2.png', null,  array('class' => 'img-responsive col-md-5 col-md-offset-4')) !!}
+<br/>
+<br/>6{!! Html::image('images/3.png', null,  array('class' => 'img-responsive col-md-5 col-md-offset-4')) !!}
+<br/>
+<br/>
+{!! Html::image('images/4.png', null, array('class' => 'img-responsive col-md-5 col-md-offset-4')) !!}
+<br/>
+<br/>
+{!! Html::image('images/5.png',  null, array('class' => 'img-responsive col-md-5 col-md-offset-4')) !!}
 @stop
